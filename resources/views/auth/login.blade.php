61cc62ef146ebf3da058eaf629d18bba4fd9017e
Roman Matviy <roman@matviy.pp.ua>
2023-01-23 23:12:09 +0200

    create register page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,3 +1,6 @@
+<?php
+return false;
+?>
 <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
@@ -16,7 +19,7 @@
             <div>
                 <x-input-label for="email" :value="__('Email')" />
 
-                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus oninvalid="invalidMsg(this);" />
 
                 <x-input-error :messages="$errors->get('email')" class="mt-2" />
             </div>
@@ -28,7 +31,7 @@
                 <x-text-input id="password" class="block mt-1 w-full"
                                 type="password"
                                 name="password"
-                                required autocomplete="current-password" />
+                                required autocomplete="current-password" oninvalid="invalidMsg(this);" />
 
                 <x-input-error :messages="$errors->get('password')" class="mt-2" />
             </div>
@@ -42,11 +45,11 @@
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
+                <!-- @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
-                @endif
+                @endif -->
 
                 <x-primary-button class="ml-3">
                     {{ __('Log in') }}
@@ -55,3 +58,5 @@
         </form>
     </x-auth-card>
 </x-guest-layout>
+
+@include('components.script');
