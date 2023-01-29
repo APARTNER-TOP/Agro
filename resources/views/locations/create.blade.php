70926c45b368513c2ce150e382776df4d6080158
Roman Matviy <roman@matviy.pp.ua>
2023-01-29 18:08:05 +0200

    page for create new location

--- resources/views/locations/create.blade.php
+++ resources/views/locations/create.blade.php
@@ -1,19 +1,34 @@
-<h1>Create New Devices</h1>
+<?php return false; ?>
+
+<h1>Create New Locations</h1>
 <div class="container">
-    <form method="POST" action="/locations/action">
+    <div class="col-xl-6" id="location_create">
+        <p>Оберіть Ваші Локації для торгівлі:</p>
+        <ul id="location_type">
+            @foreach ($locationsType as $locationType)
+            <li data-id="{{ $locationType->id}}">
+                <img src="/img/locations/{{ $locationType->id}}.png" alt="img" width="20" height="20" /> {{ $locationType->name}}
+            </li>
+            @endforeach
+        </ul>
+    </div>
 
+    <form method="POST" action="/dashboard/locations/action">
         {{ csrf_field() }}
+        @include ('components.locations.forms')
 
-        <div>
+        <!-- <div>
             <label>Device Name</label>
             <input type="text" name="name" placeholder="Device Name">
-        </div>
+        </div> -->
         <!-- <div>
             <label >Device Description</label>
             <textarea name="description" placeholder="Device Description"></textarea>
         </div> -->
         <div>
-            <input type="submit" value="Make Device">
+            <input type="submit" value="Make Location">
         </div>
     </form>
 </div>
+
+@include('components.script')
