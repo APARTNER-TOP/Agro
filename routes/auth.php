ba5971bf7a1478fcc1301cd73809a4fed7b9bac3
Roman Matviy <roman@matviy.pp.ua>
2023-01-29 18:07:23 +0200

    move code to auth

--- routes/auth.php
+++ routes/auth.php
@@ -11,6 +11,8 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
+use App\Models\Location as Location;
+
 Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
                 ->name('register');
@@ -36,24 +38,62 @@ Route::middleware('guest')->group(function () {
 });
 
 Route::middleware('auth')->group(function () {
-    Route::get('verify-email', [EmailVerificationPromptController::class, '__invoke'])
-                ->name('verification.notice');
+    // Route::get('verify-email', [EmailVerificationPromptController::class, '__invoke'])
+    //             ->name('verification.notice');
 
-    Route::get('verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke'])
-                ->middleware(['signed', 'throttle:6,1'])
-                ->name('verification.verify');
+    // Route::get('verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke'])
+    //             ->middleware(['signed', 'throttle:6,1'])
+    //             ->name('verification.verify');
 
-    Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-                ->middleware('throttle:6,1')
-                ->name('verification.send');
+    // Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+    //             ->middleware('throttle:6,1')
+    //             ->name('verification.send');
 
-    Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
-                ->name('password.confirm');
+    // Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
+    //             ->name('password.confirm');
 
-    Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
+    // Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
 
-    Route::put('password', [PasswordController::class, 'update'])->name('password.update');
+    // Route::put('password', [PasswordController::class, 'update'])->name('password.update');
 
-    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
-                ->name('logout');
+    // Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
+    //             ->name('logout');
+});
+
+
+//! admin page
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', function () {
+        // \App::setLocale('en');
+
+        // $locations = Location::all();
+        $locations = Location::select('id', 'type_id', 'company', 'address', 'lat', 'lon')->where('user_id' ,'=', Auth::user()->id)->get();
+
+        // $locationsType = DB::table('locations_type')->get();
+        $locationsType = Location::getTypes();
+
+        return view('test', compact('locations', 'locationsType'));
+        // return view('dashboard');
+    })->name('dashboard');
+
+
+    //! Admin locations page
+    // Route::get('/dashboard/locations','App\Http\Controllers\LocationsController@index')->name('locations');
+
+    // Route::get('/dashboard/locations/map','App\Http\Controllers\LocationsController@map')->name('locations/map');
+
+    Route::get('/dashboard/locations/create', function() {
+        $locationsType = Location::getTypes();
+
+        return view('locations.create', compact('locationsType'));
+    })->name('locations.create');
+
+    Route::get('/dashboard/locations/edit','App\Http\Controllers\LocationsController@edit')->name('locations.edit');
+
+    Route::get('/dashboard/locations/delete','App\Http\Controllers\LocationsController@delete')->name('locations.delete');
+
+    Route::post('/dashboard/locations/update','App\Http\Controllers\LocationsController@storeOrUpdate')->name('locations.update');
+
+    Route::post('/dashboard/locations/action','App\Http\Controllers\LocationsController@storeOrUpdate')->name('locations.action');
+
 });
--- routes/web.php
+++ routes/web.php
@@ -17,57 +17,19 @@ use App\Models\Location as Location;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('/', function () {
     // $dateOfBirth = '1987-09-09';
     // $years = Carbon::parse($dateOfBirth)->age;
-    $res       = [];
+    // $res       = [];
     // $res['dob_years'] = $years;
     // $res['dob_date'] = str_replace('-', ' ', $dateOfBirth);
 
-    return view('auth/register', $res);
-});
-
-
-// Route::get('/locations/create', 'App\Http\Controllers\LocationsController@create')->name('locations.create', function () {
-//     return view('test');
-//     // return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('locations.create');
-
-
-
-//! admin page
-Route::get('dashboard', function () {
-    // \App::setLocale('en');
-
-    // $locations = Location::all();
-    $locations = Location::select('id', 'type_id', 'company', 'address', 'lat', 'long')->where('user_id' ,'=', Auth::user()->id)->get();
-    $locationsType = DB::table('locations_type')->get();
-
-    return view('test', compact('locations', 'locationsType'));
-    // return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-
-
-//! Admin locations page
-Route::get('/dashboard/locations','App\Http\Controllers\LocationsController@index')->middleware(['auth', 'verified'])->name('locations');
-
-Route::get('/dashboard/locations/map','App\Http\Controllers\LocationsController@map')->middleware(['auth', 'verified'])->name('locations/map');
-
-Route::get('/dashboard/locations/create','App\Http\Controllers\LocationsController@create')->middleware(['auth', 'verified'])->name('locations.create');
-
-Route::get('/dashboard/locations/edit','App\Http\Controllers\LocationsController@edit')->middleware(['auth', 'verified'])->name('locations.edit');
-
-Route::get('/dashboard/locations/delete','App\Http\Controllers\LocationsController@delete')->middleware(['auth', 'verified'])->name('locations.delete');
-
-Route::post('/dashboard/locations/update','App\Http\Controllers\LocationsController@storeOrUpdate')->middleware(['auth', 'verified'])->name('locations.update');
-
-Route::post('/dashboard/locations/action','App\Http\Controllers\LocationsController@storeOrUpdate')->middleware(['auth', 'verified'])->name('locations.action');
-
+//     return view('auth/register', $res);
+// });
 
 //! settings page
 // Route::get('/admin/settings', function () {
@@ -81,4 +43,5 @@ Route::post('/dashboard/locations/action','App\Http\Controllers\LocationsControl
 //     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 // });
 
+//! admin page
 require __DIR__.'/auth.php';
