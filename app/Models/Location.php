3595c216bcedc4c0bd8859791864227b5c2ddc48
Roman Matviy <roman@matviy.pp.ua>
2023-01-29 18:09:24 +0200

    get types locations

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Location extends Model
 {
     use HasFactory;
+
+    public static function getTypes() {
+        return DB::table('locations_type')->get();
+    }
 }
