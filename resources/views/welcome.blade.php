886c6ed38b9ffe7c36af266798b84b9607540b52
Roman Matviy <roman@matviy.pp.ua>
2023-01-31 11:10:16 +0200

    add translate

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,13 @@
-@if (false || Route::has('login'))
+@if (Route::has('login'))
     <ul>
         @auth
             <li><a href="{{ url('/dashboard') }}">Dashboard</a></li>
         @else
-            <li><a href="{{ route('login') }}">Log in</a></li>
+            <li><a href="{{ route('login') }}">
+                <!-- {{ __('Log in') }} -->
+
+                Log in
+            </a></li>
 
             @if (Route::has('register'))
                 <li><a href="{{ route('register') }}">Register</a></li>
