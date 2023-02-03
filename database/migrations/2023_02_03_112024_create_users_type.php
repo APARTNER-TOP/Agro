4c2998bc3efee6be44456a0d94902ddba2764325
Roman Matviy <roman@matviy.pp.ua>
2023-02-03 13:33:51 +0200

    add users_type db table

---
+++ database/migrations/2023_02_03_112024_create_users_type.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_type', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+        });
+
+        DB::table('users_type')->insert([
+            ['name' => 'Admin'],
+            ['name' => 'Manager'],
+            ['name' => 'Фермер / Виробник'],
+            ['name' => 'Трейдер'],
+            ['name' => 'Переробник'],
+            ['name' => 'Брокер'],
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_type');
+    }
+};
