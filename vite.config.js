8ccf43993908380d06689dbe7f4a39683f71f3f1
Roman Matviy <roman@matviy.pp.ua>
2023-02-12 21:01:58 +0200

    add npm copy

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,28 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
+        }),
+
+        viteStaticCopy({
+            targets: [
+                {
+                    src: "resources/img",
+                    dest: "../", // 2️⃣
+                    // 		src: path.resolve(__dirname, './lib') + '/[!.]*', // 1️⃣
+                    // 		dest: './', // 2️⃣
+                },
+                // {
+                // 	src: 'resource/img',
+                // 	dest: '../',
+                // },
+            ],
         }),
     ],
 });
