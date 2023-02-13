33084ffe95bf3a64b793f2dac08b89b2b7b7311d
Roman Matviy <roman@matviy.pp.ua>
2023-02-13 23:35:45 +0200

    add fillable

--- app/Models/Culture.php
+++ app/Models/Culture.php
@@ -10,6 +10,9 @@ class Culture extends Model
 {
     use HasFactory;
 
+    public $timestamps = false;
+    protected $fillable = ['location_id', 'culture_type', 'offer_type', 'price', 'weight'];
+
     public static function getTypes() {
         return DB::table('culture_type')->get();
     }
